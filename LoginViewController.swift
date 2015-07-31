498610e334a325201a8c7b0db13d30823cd30eac
Shota Aizawa <someone@nowhere.com>
2015-07-30 23:00:41 -0400

    Created a new project
    a new project for FunnelTweets in Tab Bar View Controller. Added
    Twitter fabric

---
+++ LoginViewController.swift
@@ -0,0 +1,44 @@
+//
+//  LoginViewController.swift
+//  FunnelTweets
+//
+//  Created by 相澤渉太 on 2015/07/30.
+//  Copyright (c) 2015年 Shota Aizawa. All rights reserved.
+//
+
+import UIKit
+import TwitterKit
+
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let logInButton = TWTRLogInButton { (session, error) in
+            // play with Twitter session
+        }
+        logInButton.center = self.view.center
+        self.view.addSubview(logInButton)
+
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  FunnelTweets
+//
+//  Created by 相澤渉太 on 2015/07/30.
+//  Copyright (c) 2015年 Shota Aizawa. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
